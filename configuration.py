db7deee7418664fb859eaf25b853f896708beec4
d4vey-j <162891051+d4vey-j@users.noreply.github.com>
2024-04-26 15:43:45 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Клименко Сергей, 15-я когорта - Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://bef5ee68-4c7f-4fc1-adae-348c0932f383.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
